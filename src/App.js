d8f02e1e0c1ca4c1fc67217c34c9bc6b62deab77
Collin Newman <collinnewman12@gmail.com>
2022-10-30 12:43:09 -0500

    add graph switching

--- src/App.js
+++ src/App.js
@@ -8,33 +8,33 @@ import NavigationBar from './Nav'
 // example on how to use map:
 // https://github.com/gabidavila/react-usa-map-demo/blob/master/src/App.js
 
-var changeable = "//www.eia.gov/opendata/v1/embed/iframe.php?api_key=E0qs2v71QQZScSd0Ige5auJ1nnrk55BgsEdBbb7a&series_id=PET.EMA_EPJK_PTG_NUS_DPG.A;PET.EMA_EPJK_PWG_NUS_DPG.A;PET.EMA_EPLLPA_PTG_NUS_DPG.A;PET.EMA_EPLLPA_PWG_NUS_DPG.A;PET.EMA_EPM0_PTG_NUS_DPG.A;PET.EMA_EPM0_PWG_NUS_DPG.A;PET.EMA_EPPK_PTG_NUS_DPG.A;PET.EMA_EPPK_PWG_NUS_DPG.A;PET.EMA_EPPV_PTG_NUS_DPG.A;PET.EMA_EPPV_PWG_NUS_DPG.A";
+const API_KEY = 'E0qs2v71QQZScSd0Ige5auJ1nnrk55BgsEdBbb7a';
 
+var texas = "//www.eia.gov/opendata/v1/embed/iframe.php?api_key=" + API_KEY + '&series_id=PET.EMA_EPJK_PTG_NUS_DPG.A;PET.EMA_EPJK_PWG_NUS_DPG.A;PET.EMA_EPLLPA_PTG_NUS_DPG.A;PET.EMA_EPLLPA_PWG_NUS_DPG.A;PET.EMA_EPM0_PTG_NUS_DPG.A;PET.EMA_EPM0_PWG_NUS_DPG.A;PET.EMA_EPPK_PTG_NUS_DPG.A;PET.EMA_EPPK_PWG_NUS_DPG.A;PET.EMA_EPPV_PTG_NUS_DPG.A;PET.EMA_EPPV_PWG_NUS_DPG.A';
+var alaska = "//www.eia.gov/opendata/v1/embed/iframe.php?api_key=" + API_KEY + "&series_id=PET.RCRR9907SAK_1.A;PET.RL2R9911SAK_1.A;PET.RNGR9908SAK_1.A;PET.RNGR9909SAK_1.A;PET.RNGR9910SAK_1.A";
+var california = "//www.eia.gov/opendata/v1/embed/iframe.php?api_key=" + API_KEY + "&series_id=PET.EMA_EPJK_PTG_SCA_DPG.A;PET.EMA_EPJK_PWG_SCA_DPG.A;PET.EMA_EPM0_PWG_SCA_DPG.A;PET.EMA_EPPK_PTG_SCA_DPG.A;PET.EMA_EPPK_PWG_SCA_DPG.A;PET.EMA_EPPV_PTG_SCA_DPG.A;PET.EMA_EPPV_PWG_SCA_DPG.A";
 
 class App extends Component {
 
   
 
   mapHandler = (event) => {
-    this.setState({src: changeable});
+    if(event.target.dataset.name == "TX"){
+      this.setState({src: texas});
+    }
+    if(event.target.dataset.name == "AK") {
+      this.setState({src: alaska});
+    }
+    if(event.target.dataset.name == "CA") {
+      this.setState({src: california});
+    }
+    
   };
 
   state = {
-    src: "//www.eia.gov/opendata/v1/embed/iframe.php?api_key=E0qs2v71QQZScSd0Ige5auJ1nnrk55BgsEdBbb7a&series_id=PET.RCRR9907SAK_1.A;PET.RL2R9911SAK_1.A;PET.RNGR9908SAK_1.A;PET.RNGR9909SAK_1.A;PET.RNGR9910SAK_1.A"
+    src: alaska
   }
   
-  // statesFilling = () => {
-  //   return {
-  //       "NJ": {
-  //       fill: "navy",
-  //       clickHandler: () => alert("Custom callback for the NJ state")
-  //     },
-  //     "NY": {
-  //       fill: "#CC0000"
-  //     }
-  //   };
-  // };
- 
   render() 
   {
     return (
