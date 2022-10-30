6e599879a30b0557ef46714f8c457b1f4ad74c5f
ethan <ethanm113@gmail.com>
2022-10-30 10:00:10 -0500

    fixed new nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,34 +1,88 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
 
+// import {
+//     Container, Row, Col, Form, Input, Button, Navbar, Nav,
+//     NavbarBrand, NavLink, NavItem, UncontrolledDropdown,
+//     DropdownToggle, DropdownMenu, DropdownItem, NavbarText
+//   } from 'reactstrap';
+
+
+// class NavigationBar extends Component {
+
+//     render() {
+//         return (
+//             <div>
+//                 <Navbar>
+//                     <NavbarBrand style={{color: 'white'}} href="/">Oil and Gas In the United States</NavbarBrand>
+//                     <Nav className="me-auto" navbar>
+//                         <NavItem>
+//                             <NavLink style={{color: 'white'}} href="/about">About</NavLink>
+//                             </NavItem>
+//                         <NavItem>
+//                             <NavLink style={{color: 'white'}} href="https://github.com/jetskipenguin/team-one">
+//                                 GitHub
+//                             </NavLink>
+//                         </NavItem>
+//                     </Nav>
+//                 </Navbar>
+//             </div>
+//         );
+//     }
+// }
+
+// export default NavigationBar;
+
+import React, { useState } from 'react';
 import {
-    Container, Row, Col, Form, Input, Button, Navbar, Nav,
-    NavbarBrand, NavLink, NavItem, UncontrolledDropdown,
-    DropdownToggle, DropdownMenu, DropdownItem, NavbarText
-  } from 'reactstrap';
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
 
+function Example(args) {
+  const [isOpen, setIsOpen] = useState(false);
 
-class NavigationBar extends Component {
+  const toggle = () => setIsOpen(!isOpen);
 
-    render() {
-        return (
-            <div>
-                <Navbar>
-                    <NavbarBrand style={{color: 'white'}} href="/">Oil and Gas In the United States</NavbarBrand>
-                    <Nav className="me-auto" navbar>
-                        <NavItem>
-                            <NavLink style={{color: 'white'}} href="/about">About</NavLink>
-                            </NavItem>
-                        <NavItem>
-                            <NavLink style={{color: 'white'}} href="https://github.com/jetskipenguin/team-one">
-                                GitHub
-                            </NavLink>
-                        </NavItem>
-                    </Nav>
-                </Navbar>
-            </div>
-        );
-    }
+  return (
+    <div>
+      <Navbar {...args}>
+        <NavbarBrand href="/">Oil and Gas Usage In the United States</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="https://github.com/jetskipenguin/team-one">GitHub</NavLink>
+            </NavItem>
+            <NavItem>
+            </NavItem>
+            {/* <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown> */}
+          </Nav>
+          {/* <NavbarText>Simple Text</NavbarText> */}
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
-export default NavigationBar;
+export default Example;
